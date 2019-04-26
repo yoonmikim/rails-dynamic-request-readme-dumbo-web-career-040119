8edaf58c03232650dd403acce6c4e17f18d5bee8
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-26 18:43:34 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get 'about', to: "static#about" 
-  get 'posts/:id', to: 'posts#show'
+  resources :posts, only: :show
   
 end
